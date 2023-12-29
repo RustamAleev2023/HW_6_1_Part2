6efcc3df04791a570351f5c7849f1c28eed7b001
RustamAleev2023 <ridermix@yandex.ru>
2023-12-29 13:22:56 +0200

    Task4 done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 //        task1();
 //        task2();
-        task3();
+//        task3();
+        task4();
 
     }
 
@@ -26,12 +28,12 @@ public class Main {
         double[] angles = new double[]{30, 90};
         int type; // 1-degrees, 0 - radians
 
-        System.out.println("SumSinuses = " + getSumSinuses(angles,1));
-        System.out.println("SumCosines = " + getSumCosines(angles,1));
-        System.out.println("SubtractSinuses = " + getSubtractSinuses(angles,1));
-        System.out.println("SubtractCosines = " + getSubtractCosines(angles,1));
-        System.out.println("MultiplySinuses = " + getMultiplySinuses(angles,1));
-        System.out.println("MultiplyCosines = " + getMultiplyCosines(angles,1));
+        System.out.println("SumSinuses = " + getSumSinuses(angles, 1));
+        System.out.println("SumCosines = " + getSumCosines(angles, 1));
+        System.out.println("SubtractSinuses = " + getSubtractSinuses(angles, 1));
+        System.out.println("SubtractCosines = " + getSubtractCosines(angles, 1));
+        System.out.println("MultiplySinuses = " + getMultiplySinuses(angles, 1));
+        System.out.println("MultiplyCosines = " + getMultiplyCosines(angles, 1));
 
 
     }
@@ -39,9 +41,9 @@ public class Main {
     public static double getSumSinuses(double[] angles, int type) {
         double sumSinuses = 0;
         for (int i = 0; i < angles.length; i++) {
-            if(type == 1){
+            if (type == 1) {
                 sumSinuses += Math.sin(Math.toRadians(angles[i]));
-            } else if (type == 0){
+            } else if (type == 0) {
                 sumSinuses += Math.sin(angles[i]);
             } else {
                 System.out.println("Вы неверно выбрали размерность углов");
@@ -49,12 +51,13 @@ public class Main {
         }
         return sumSinuses;
     }
+
     public static double getSumCosines(double[] angles, int type) {
         double sumCosines = 0;
         for (int i = 0; i < angles.length; i++) {
-            if(type == 1){
+            if (type == 1) {
                 sumCosines += Math.cos(Math.toRadians(angles[i]));
-            } else if (type == 0){
+            } else if (type == 0) {
                 sumCosines += Math.cos(angles[i]);
             } else {
                 System.out.println("Вы неверно выбрали размерность углов");
@@ -62,12 +65,13 @@ public class Main {
         }
         return sumCosines;
     }
+
     public static double getSubtractSinuses(double[] angles, int type) {
         double subtractSinuses = 0;
         for (int i = 0; i < angles.length; i++) {
-            if(type == 1){
+            if (type == 1) {
                 subtractSinuses -= Math.sin(Math.toRadians(angles[i]));
-            } else if (type == 0){
+            } else if (type == 0) {
                 subtractSinuses -= Math.sin(angles[i]);
             } else {
                 System.out.println("Вы неверно выбрали размерность углов");
@@ -75,12 +79,13 @@ public class Main {
         }
         return subtractSinuses;
     }
+
     public static double getSubtractCosines(double[] angles, int type) {
         double subtractCosines = 0;
         for (int i = 0; i < angles.length; i++) {
-            if(type == 1){
+            if (type == 1) {
                 subtractCosines -= Math.cos(Math.toRadians(angles[i]));
-            } else if (type == 0){
+            } else if (type == 0) {
                 subtractCosines -= Math.cos(angles[i]);
             } else {
                 System.out.println("Вы неверно выбрали размерность углов");
@@ -88,12 +93,13 @@ public class Main {
         }
         return subtractCosines;
     }
+
     public static double getMultiplySinuses(double[] angles, int type) {
         double multiplySinuses = 1;
         for (int i = 0; i < angles.length; i++) {
-            if(type == 1){
+            if (type == 1) {
                 multiplySinuses *= Math.sin(Math.toRadians(angles[i]));
-            } else if (type == 0){
+            } else if (type == 0) {
                 multiplySinuses *= Math.sin(angles[i]);
             } else {
                 System.out.println("Вы неверно выбрали размерность углов");
@@ -101,12 +107,13 @@ public class Main {
         }
         return multiplySinuses;
     }
+
     public static double getMultiplyCosines(double[] angles, int type) {
         double multiplyCosines = 1;
         for (int i = 0; i < angles.length; i++) {
-            if(type == 1){
+            if (type == 1) {
                 multiplyCosines *= Math.cos(Math.toRadians(angles[i]));
-            } else if (type == 0){
+            } else if (type == 0) {
                 multiplyCosines *= Math.cos(angles[i]);
             } else {
                 System.out.println("Вы неверно выбрали размерность углов");
@@ -122,7 +129,7 @@ public class Main {
     // итерационного алгоритмов. Во сколько раз будет отличаться время выполнения
     // и как это отношение будет зависеть от числа, факториал которого рассчитывается?
     // Программа должна вывести на экран результат анализа.
-    public static void task2(){
+    public static void task2() {
 
         int n = 1000;
 
@@ -138,11 +145,12 @@ public class Main {
 
         System.out.println("timeFactByRec = " + timeFactByRec);
         System.out.println("timeFactByIt = " + timeFactByIt);
-        System.out.println("Итерационный алгоритм быстрее в " + (timeFactByRec/timeFactByIt) + " раз" );
+        System.out.println("Итерационный алгоритм быстрее в " + (timeFactByRec / timeFactByIt) + " раз");
 
 
     }
-    public static long factorialByRecursion(int n){
+
+    public static long factorialByRecursion(int n) {
         long result;
         if (n == 1) {
             result = 1;
@@ -151,7 +159,8 @@ public class Main {
         }
         return result;
     }
-    public static long factorialByIteration(int n){
+
+    public static long factorialByIteration(int n) {
         long result = 1;
         for (int i = 1; i <= n; i++) {
             result = result * i;
@@ -167,30 +176,202 @@ public class Main {
     // элементов, переданный в качестве аргумента в этот метод. Сортировка должна быть устойчивой и
     // иметь естественное поведение. (Не использовать Arrays.sort). Программа должна вывести на экран
     // исходный и отсортированный массив.
-    public static void task3(){
-        int[] array = new int[]{1,3,2,5,4,7,6,9,8,10};
+    public static void task3() {
+        int[] array = new int[]{1, 3, 2, 5, 4, 7, 6, 9, 8, 10};
         print(array);
         sort(array);
         print(array);
 
     }
-    public static void print(int[] arr){
+
+    public static void print(int[] arr) {
         System.out.println(Arrays.toString(arr));
     }
-    public static void sort(int[] arr){
+
+    public static void sort(int[] arr) {
         boolean isSorted = false;
         int temp;
-        while(!isSorted) {
+        while (!isSorted) {
             isSorted = true;
-            for (int i = 0; i < arr.length-1; i++) {
-                if(arr[i] < arr[i+1]){
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] < arr[i + 1]) {
                     isSorted = false;
 
                     temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
                 }
             }
         }
     }
+
+
+    //Task4
+    //Задание 4
+    //В массиве хранится n явно заданных текстовых строк.
+    // Создать программу, которая отсортирует и выведет на экран строки в алфавитном порядке.
+    // Указание: прежде всего надо создать метод, устанавливающий отношения порядка для двух строк,
+    // переданных методу через аргументы. Программа должна вывести строки в начальном и отсортированном
+    // порядке.(2 балла)
+    //Дополнительно 1 балл за генерацию уникальных строк реализованных в виде метода.
+    public static void task4() {
+        String array[] = {"Петров", "Сидоров", "Иванов"};
+//        String array[] = {"собака", "выдра", "кот", "акула"};
+
+
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (compare(array[i], array[i + 1]) < 0) {
+                String temp = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = temp;
+            }
+        }
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static int compare(String s1, String s2) {
+        return s1.compareTo(s2);
+    }
+
+    public static void printStringArray(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    //Task5
+    //Задание 5
+    //Королю нужно убить дракона, но в его казне мало средств для покупки армии. Нужно создать программу используя методы, которая поможет рассчитать минимальное количество копейщиков, которое необходимо, чтобы убить дракона. Данные здоровья и атаки дракона и одного копейщика вводятся с клавиатуры.
+    //  Защита, меткость и т. п. не учитывать. Копейщики наносят удар первыми(общий нанесенный урон – это сумма атак всех живых копейщиков). Если атака дракона превышает значение жизни копейщика (например у копейщика жизни 10, а атака 15),  то умирает несколько копейщиков, а оставшийся урон идет одному из копейщиков.
+    //Например, жизнь дракона 500, атака 55, жизнь одного копейщика 10, атака 10, а количество копейщиков при данных условиях 20.
+    //Лог боя для данного примера должен выглядеть так:
+    //Копейщики атакуют (урон 200) – у дракона осталось 300 жизней
+    //Дракон атакует – осталось 15 копейщиков, один из которых ранен (осталось 5 жизней)
+    //Копейщики атакуют – у дракона осталось 150 жизней
+    //Дракон атакует – осталось 9 копейщиков
+    //Копейщики атакуют – у дракона осталось 60 жизней
+    //Дракон атакует – осталось 4 копейщика , один из которых ранен (осталось 5 жизней)
+    // Копейщики атакуют – у дракона осталось 20 жизней
+    //Дракон атакует  и побеждает
+    public static void task5() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите здоровье дракона");
+        int dragonHealth = scanner.nextInt();
+        System.out.println("Введите атаку дракона");
+        int dragonFight = scanner.nextInt();
+        System.out.println("Введите здоровье копейщика");
+        int spearmanHealth = scanner.nextInt();
+        System.out.println("Введите атаку копейщика");
+        int spearmanFight = scanner.nextInt();
+
+//        int dragonHealth = 500;
+//        int dragonFight = 50;
+//        int spearmanHealth = 10;
+//        int spearmanFight = 10;
+
+        int lowCountSpearman = 1;
+        int maxCountSpearman = dragonHealth / spearmanFight;
+
+        //поиск минимального кол-ва копейщиков
+        while (lowCountSpearman < maxCountSpearman) {
+            int mid = lowCountSpearman + (maxCountSpearman - lowCountSpearman) / 2;
+            if (fight(dragonHealth, dragonFight, spearmanHealth, spearmanFight, mid)) {
+                maxCountSpearman = mid;
+            } else {
+                lowCountSpearman = mid + 1;
+            }
+        }
+        System.out.println("Минимальное кол-во копейщиков для уничтожения дракона равно: " + lowCountSpearman);
+    }
+
+    public static boolean fight(int dragonHealth, int dragonFight, int spearmanHealth, int spearmanFight, int countSpearman) {
+        int sumSpearmanHealth = spearmanHealth * countSpearman;
+
+        while (true) {
+            dragonHealth -= spearmanFight * countSpearman;
+            if (dragonHealth <= 0) {
+                return true; //Дракон проиграл
+            }
+            sumSpearmanHealth -= dragonFight;
+            if (sumSpearmanHealth <= 0) {
+                return false; //Копейщики проиграли
+            }
+            countSpearman = sumSpearmanHealth / spearmanHealth;
+
+        }
+    }
+
+    //Task6
+    //Напишите рекурсивную функцию, проверяющую правильность расстановки скобок в строке введенной с клавиатуры. При правильной расстановке выполняются условия:
+    //a.   количество открывающих и закрывающих скобок равно.
+    //b.   внутри любой пары открывающая – соответствующая закрывающая скобка,
+    //скобки расставлены правильно.
+    //В строке могут присутствовать скобки как круглые, так и квадратные скобки (и др. символы). Каждой открывающей скобке соответствует закрывающая того же типа (круглой – круглая, квадратной- квадратная). Напишите рекурсивную функцию, проверяющую правильность расстановки скобок в строке.
+    //Пример неправильной расстановки: ( [ ) ].
+    //Пример правильных входных данных (a[b](f[(g)(g)]))
+    //Программа должна вывести результат в виде строки, примеры
+    //•  Правильная строка
+    //•  Ошибка отсутствие (
+    //•  Ошибка отсутствие [
+    static int countSquareBracket;
+    static int countRoundBracket;
+
+    public static void task6() {
+
+
+        String s = "(a[b](f[(g)(g)]))";
+
+        isOk(s);
+    }
+
+    public static boolean isOk(String s) {
+        countRoundBracket = 0;
+        countSquareBracket = 0;
+
+        //Первый символ не должен быть ) или ]
+        String firstSymbol = s.substring(0, 1);
+        if (firstSymbol.equals(")") || firstSymbol.equals("]")) {
+            System.out.println("Ошибка. Строка не может начинаться с закрывающихся скобок");
+            return false;
+        }
+        //проверяем посимвольно строку на правильную последовательность скобок
+        for (int i = 0; i < s.length(); i++) {
+            String symbol = s.substring(i, i + 1);
+            switch (symbol) {
+                case "(" -> countRoundBracket++;
+                case ")" -> countRoundBracket--;
+                case "[" -> countSquareBracket++;
+                case "]" -> countSquareBracket--;
+            }
+        }
+
+
+        if (countRoundBracket > 0) {
+            System.out.println("Ошибка отсутствие )");
+            return false;
+        } else if (countRoundBracket < 0) {
+            System.out.println("Ошибка отсутствие (");
+            return false;
+        } else if (countSquareBracket > 0) {
+            System.out.println("Ошибка отсутствие ]");
+            return false;
+        } else if (countSquareBracket < 0) {
+            System.out.println("Ошибка отсутствие [");
+            return false;
+        } else {
+            System.out.println("Правильная строка");
+            return true;
+        }
+    }
+
+
 }
